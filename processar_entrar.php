9ea7c27fd7ec45e4e086d7dcb8ca6aac0a3cd2fc
MateteNtaluma <133655439+MateteNtaluma@users.noreply.github.com>
2023-06-12 12:31:54 +0100

    Update processar_entrar.php

--- processar_entrar.php
+++ processar_entrar.php
@@ -3,7 +3,14 @@ $email=$_POST['email'];
 $senha=$_POST['senha'];
 
 if (isset($email) && isset($senha)){ 
-  $liga=
+  $liga=mysqli_connect('localhost','root','root','RTL');
+  $verifica = mysqli_query($liga,"SELECT * FROM utilizadores WHERE email='$email'");
+  if (mysqli_num_rows($verifica) > 0 ){
+    echo "Utilizador Existente!!!";
+  }
+  else {
+    echo "<h1>Utilizador não Existente</h1>";
+  }
 }
 
 ?>

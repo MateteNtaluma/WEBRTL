73cca649703660a566e6aba8dad0f8800626f90e
MateteNtaluma <133655439+MateteNtaluma@users.noreply.github.com>
2023-06-19 12:23:56 +0100

    Update processar_registo.php

--- processar_registo.php
+++ processar_registo.php
@@ -7,7 +7,7 @@ $email = $_POST['email_registo'];
 $pass = $_POST['senha_registo'];
 
 if (isset($nome) && isset($telefone) && isset($nascimento) && isset($email) && isset($pass)) {
-  $liga = mysqli_connect('localhost', 'root', '', 'RTL');
+  $liga = mysqli_connect('localhost', 'root', 'root', 'RTL');
   $sql = "INSERT INTO utilizadores (id, email, nome, telefone, data_nascimento, pass) VALUES (NULL, '$email','$nome','$telefone','$nascimento','$pass')";
   if ($liga->query($sql) === TRUE) {
     // Mudar para a página que pretende ir

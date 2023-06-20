b2a1888e1715089972a0fddd66c734eef0f84bb3
MateteNtaluma <133655439+MateteNtaluma@users.noreply.github.com>
2023-06-20 14:13:49 +0100

    Update gps.php

--- gps.php
+++ gps.php
@@ -1,12 +1,12 @@
 <?php
-$date=$_GET['date'];
+$data=$_GET['data'];
 $long=$_GET['longitude'];
 $lat=$_GET['latitude'];
 
 if (isset($data)){
     $liga = mysqli_connect('localhost', 'root', 'root', 'RTL');
-    $verifica = mysqli_query($liga, "INSERT INTO GPS (date,longitude,latitude) values ($date,$long,$lat)");
-    echo  "INSERT INTO GPS (date,longitude,latitude) values ($data,$long,$lat)";
+    $verifica = mysqli_query($liga, "INSERT INTO GPS (data,longitude,latitude) values ('$data','$long','$lat')");
+    echo  "INSERT INTO GPS (data,longitude,latitude) values ('$data','$long','$lat')";
 }
-echo $date . $long . $lat;
+echo $data . $long . $lat;
 ?>

c49cc5c3d05731d58d418c70f4112c822798cc0f
MateteNtaluma <133655439+MateteNtaluma@users.noreply.github.com>
2023-06-20 13:52:31 +0100

    Update gps.php

--- gps.php
+++ gps.php
@@ -4,7 +4,7 @@ $long=$_GET['longitude'];
 $lat=$_GET['latitude'];
 
 if (isset($data)){
-    $liga = mysqli_connect('localhost', 'root', '', 'RTL');
+    $liga = mysqli_connect('localhost', 'root', 'root', 'RTL');
     $verifica = mysqli_query($liga, "INSERT INTO GPS (data,longitude,latitude) values ($data,$long,$lat)");
 }
 ?>

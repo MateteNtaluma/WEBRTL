bec1667891327527c61446828886606000d15f4a
MateteNtaluma <133655439+MateteNtaluma@users.noreply.github.com>
2023-06-25 17:48:51 +0100

    Update gps.php

--- gps.php
+++ gps.php
@@ -1,18 +1,61 @@
 <?php
-$nome=$_GET['nome'];
-$data=$_GET['data'];
-$long=$_GET['longitude'];
-$lat=$_GET['latitude'];
 
-if (isset($data)){
-  echo "1";
+$nome = $_GET['nome'];
+
+$data = $_GET['data'];
+
+$long = $_GET['longitude'];
+
+$lat = $_GET['latitude'];
+
+$hour = $_GET['hour'];
+
+$minute = $_GET['minute'];
+
+$second = $_GET['second'];
+
+ 
+
+if (isset($data)) {
+
     $liga = mysqli_connect('localhost', 'root', 'root', 'RTL');
-  echo "2";
-    $sql = mysqli_query($liga, "INSERT INTO gps (Data,Longitude,Latitude) values ('$data','$long','$lat')");
-  echo "3";
+
+ 
+
+    $data = mysqli_real_escape_string($liga, $data);
+
+    $long = mysqli_real_escape_string($liga, $long);
+
+    $lat = mysqli_real_escape_string($liga, $lat);
+
+    $hour = mysqli_real_escape_string($liga, $hour);
+
+    $minute = mysqli_real_escape_string($liga, $minute);
+
+    $second = mysqli_real_escape_string($liga, $second);
+
+ 
+
+    $sql = "INSERT INTO gps (Data, Longitude, Latitude, Hour, Minute, Second) VALUES ('$data', '$long', '$lat', '$hour', '$minute', '$second')";
+
+ 
+
     if ($liga->query($sql) === TRUE) {
-    echo "Dados inseridos";
-   } 
+
+        echo "Dados inseridos com sucesso";
+
+    } else {
+
+        echo "Erro ao inserir dados: " . $liga->error;
+
+    }
+
+} else {
+
+    echo "Dados inválidos";
+
 }
-echo $data . $long . $lat;
+
 ?>
+
+
